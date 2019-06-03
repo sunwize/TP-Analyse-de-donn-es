1ba3246525ca4a2c6ca3fd2e0e9c3594edbb27bc
sunwize <molpick98@gmail.com>
2019-06-03 14:01:43 +0200

    Ajout des commentaires

--- TP3.R
+++ TP3.R
@@ -1,3 +1,7 @@
+#Colin Clisson
+
+#Question 1 :
+
 Ax<-runif(100, 0, 1)
 Ay<-runif(100, 0, 1)
 
@@ -24,6 +28,8 @@ points(Ax, Ay, col="red")
 points(Bx, By, col="blue")
 points(Cx, Cy, col="green")
 
+#Question 2 :
+
 D<-dist(A, method = "euclidean")
 D<-as.matrix(D)
 
@@ -32,11 +38,11 @@ rownames(A)<-c(1:nrow(A))
 #D<-dist(A, method = "euclidean")
 #D<-as.matrix(D)
 
-while(nrow(D) >= 4) {
+while(nrow(D) >= 4) { # Tant qu'il y a plus de 3 classes
   min<-D[2,1]
   di<-2
   dj<-1
-  for(i in 2:nrow(D)) {
+  for(i in 2:nrow(D)) { # On cherche le minimum de la matrice
     for(j in 1:(i-1)) {
       if(min > D[i, j]) {
         min<-D[i, j]
@@ -45,14 +51,19 @@ while(nrow(D) >= 4) {
       }
     }
   }
+  # On calcule les moyennes
   moyX<-(A[di,1] + A[dj,1]) / 2
   moyY<-(A[di,2] + A[dj,2]) / 2
+  # On met a jour les classes
   A[dj,1]<-moyX
   A[dj,2]<-moyY
+  # On renomme la classe pour garder une trace des elements qui ont ete ajoutes
   rownames(A)[dj]<-paste(rownames(A)[dj], rownames(A)[di])
+  # On supprime la ligne de la classe qui a ete ajoutee
   A<-A[-di,]
+  # On recalcule la matrice des distances
   D<-dist(A, method = "euclidean")
   D<-as.matrix(D)
 }
-rownames(A)
-points(A, col="purple")
+rownames(A) # Affichage des classes
+points(A, col="purple") # Affichage des barycentres des classes
